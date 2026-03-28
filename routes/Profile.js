16f197bffbc95a9e9dad613ddef367adbefcdf5c
Zeeshan Ali Zafar <zeeshanalizafar53@gmail.com>
2026-03-28 13:45:46 +0500

    NEw

--- routes/Profile.js
+++ routes/Profile.js
@@ -5,16 +5,7 @@ import path from 'path';
 
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'upload/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    const ext = path.extname(file.originalname)
-    cb(null, file.fieldname + '-' + uniqueSuffix +ext)
-  }
-})
+const storage = multer.memoryStorage();
 
 const upload = multer({storage:storage})
 
